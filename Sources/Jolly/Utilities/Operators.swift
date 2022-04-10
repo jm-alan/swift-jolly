7114ff071b4e59db4799e83c7c7997dd489b5856
jm <j.maxwell.alan@gmail.com>
2022-04-10 16:42:56 -0400

    overload new operators for new discrete comparators

--- Sources/Jolly/Utilities/Operators.swift
+++ Sources/Jolly/Utilities/Operators.swift
@@ -1,14 +1,94 @@
 prefix operator ==
-prefix operator <<
-prefix operator >>
+prefix operator <~
+prefix operator >~
 prefix operator >=
 prefix operator <=
 
 @inlinable
 public func == <T, C>(
     _ lhs: KeyPath<T, C>,
+    _ rhs: KeyPath<T, C>
+) -> KeyComparator<T, C> where C: Comparable {
+    .init(lhs: lhs, rhs: rhs)
+}
+
+@inlinable
+public func < <T, C>(
+    _ lhs: KeyPath<T, C>,
+    _ rhs: KeyPath<T, C>
+) -> KeyComparator<T, C> where C: Comparable {
+    .init(lhs: lhs, rhs: rhs, method: .lesser)
+}
+
+@inlinable
+public func <= <T, C>(
+    _ lhs: KeyPath<T, C>,
+    _ rhs: KeyPath<T, C>
+) -> KeyComparator<T, C> where C: Comparable {
+    .init(lhs: lhs, rhs: rhs, method: .lesserEqual)
+}
+
+@inlinable
+public func > <T, C>(
+    _ lhs: KeyPath<T, C>,
+    _ rhs: KeyPath<T, C>
+) -> KeyComparator<T, C> where C: Comparable {
+    .init(lhs: lhs, rhs: rhs, method: .greater)
+}
+
+@inlinable
+public func >= <T, C>(
+    _ lhs: KeyPath<T, C>,
+    _ rhs: KeyPath<T, C>
+) -> KeyComparator<T, C> where C: Comparable {
+    .init(lhs: lhs, rhs: rhs, method: .greaterEqual)
+}
+
+@inlinable
+public func == <C>(
+    _ lhs: C,
     _ rhs: C
-) -> Comparator<T, C> where C: Comparable {
+) -> ValueComparator<C> where C: Comparable {
+    .init(lhs: lhs, rhs: rhs)
+}
+
+@inlinable
+public func < <C>(
+    _ lhs: C,
+    _ rhs: C
+) -> ValueComparator<C> where C: Comparable {
+    .init(lhs: lhs, rhs: rhs, method: .lesser)
+}
+
+@inlinable
+public func <= <C>(
+    _ lhs: C,
+    _ rhs: C
+) -> ValueComparator<C> where C: Comparable {
+    .init(lhs: lhs, rhs: rhs, method: .lesserEqual)
+}
+
+@inlinable
+public func > <C>(
+    _ lhs: C,
+    _ rhs: C
+) -> ValueComparator<C> where C: Comparable {
+    .init(lhs: lhs, rhs: rhs, method: .greater)
+}
+
+@inlinable
+public func >= <C>(
+    _ lhs: C,
+    _ rhs: C
+) -> ValueComparator<C> where C: Comparable {
+    .init(lhs: lhs, rhs: rhs, method: .greaterEqual)
+}
+
+@inlinable
+public func == <T, C>(
+    _ lhs: KeyPath<T, C>,
+    _ rhs: C
+) -> KeyValComparator<T, C> where C: Comparable {
     .init(lhs: lhs, rhs: rhs)
 }
 
@@ -16,7 +96,7 @@ public func == <T, C>(
 public func < <T, C>(
     _ lhs: KeyPath<T, C>,
     _ rhs: C
-) -> Comparator<T, C> where C: Comparable {
+) -> KeyValComparator<T, C> where C: Comparable {
     .init(lhs: lhs, rhs: rhs, method: .lesser)
 }
 
@@ -24,7 +104,7 @@ public func < <T, C>(
 public func <= <T, C>(
     _ lhs: KeyPath<T, C>,
     _ rhs: C
-) -> Comparator<T, C> where C: Comparable {
+) -> KeyValComparator<T, C> where C: Comparable {
     .init(lhs: lhs, rhs: rhs, method: .lesserEqual)
 }
 
@@ -32,7 +112,7 @@ public func <= <T, C>(
 public func > <T, C>(
     _ lhs: KeyPath<T, C>,
     _ rhs: C
-) -> Comparator<T, C> where C: Comparable {
+) -> KeyValComparator<T, C> where C: Comparable {
     .init(lhs: lhs, rhs: rhs, method: .greater)
 }
 
@@ -40,7 +120,7 @@ public func > <T, C>(
 public func >= <T, C>(
     _ lhs: KeyPath<T, C>,
     _ rhs: C
-) -> Comparator<T, C> where C: Comparable {
+) -> KeyValComparator<T, C> where C: Comparable {
     .init(lhs: lhs, rhs: rhs, method: .greaterEqual)
 }
 
@@ -52,7 +132,7 @@ public prefix func == <C>(_ rhs: C) -> ((C) -> Bool) where C: Comparable {
 }
 
 @inlinable
-public prefix func << <C>(_ rhs: C) -> ((C) -> Bool) where C: Comparable {
+public prefix func <~ <C>(_ rhs: C) -> ((C) -> Bool) where C: Comparable {
     return { (comparable: C) in
         comparable < rhs
     }
@@ -66,7 +146,7 @@ public prefix func <= <C>(_ rhs: C) -> ((C) -> Bool) where C: Comparable {
 }
 
 @inlinable
-public prefix func >> <C>(_ rhs: C) -> ((C) -> Bool) where C: Comparable {
+public prefix func >~ <C>(_ rhs: C) -> ((C) -> Bool) where C: Comparable {
     return { (comparable: C) in
         comparable > rhs
     }
