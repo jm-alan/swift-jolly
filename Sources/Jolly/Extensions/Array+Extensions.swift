b7f9961f4c31cbceae7263bdc4d369ba1f26e50a
jm <j.maxwell.alan@gmail.com>
2022-04-17 15:09:29 -0400

    use new reduce method in indexed methods

--- Sources/Jolly/Extensions/Array+Extensions.swift
+++ Sources/Jolly/Extensions/Array+Extensions.swift
@@ -366,9 +366,8 @@ public extension Array {
     func indexed<H>(
         by keySelector: (Element) -> H
     ) -> [H: Element] where H: Hashable {
-        var indexed: [H: Element] = [:]
-        forEach { indexed[keySelector($0)] = $0 }
-        return indexed
+        var indexed = [H: Element]()
+        return reduce(&indexed) { $0[keySelector($1)] = $1 }
     }
 
     @inlinable
@@ -376,18 +375,16 @@ public extension Array {
         by keySelector: (Element) -> KeyType,
         valueSelector: (Element) -> ValueType
     ) -> [KeyType: ValueType] where KeyType: Hashable {
-        var indexed: [KeyType: ValueType] = [:]
-        forEach { indexed[keySelector($0)] = valueSelector($0) }
-        return indexed
+        var indexed = [KeyType: ValueType]()
+        return reduce(&indexed) { $0[keySelector($1)] = valueSelector($1) }
     }
 
     @inlinable
     func indexed<H>(
         by keyPath: KeyPath<Element, H>
     ) -> [H: Element] where H: Hashable {
-        var indexed: [H: Element] = [:]
-        forEach { indexed[$0[keyPath: keyPath]] = $0 }
-        return indexed
+        var indexed = [H: Element]()
+        return reduce(&indexed) { $0[$1[keyPath: keyPath]] = $1 }
     }
 
     @inlinable
@@ -395,9 +392,8 @@ public extension Array {
         by keyPath: KeyPath<Element, KeyType>,
         valueSelector: (Element) -> ValueType
     ) -> [KeyType: ValueType] where KeyType: Hashable {
-        var indexed: [KeyType: ValueType] = [:]
-        forEach { indexed[$0[keyPath: keyPath]] = valueSelector($0) }
-        return indexed
+        var indexed = [KeyType: ValueType]()
+        return reduce(&indexed) { $0[$1[keyPath: keyPath]] = valueSelector($1) }
     }
 
     @inlinable
@@ -405,9 +401,8 @@ public extension Array {
         by keyPath: KeyPath<Element, KeyType>,
         extracting valuePath: KeyPath<Element, ValueType>
     ) -> [KeyType: ValueType] where KeyType: Hashable {
-        var indexed: [KeyType: ValueType] = [:]
-        forEach { indexed[$0[keyPath: keyPath]] = $0[keyPath: valuePath] }
-        return indexed
+        var indexed = [KeyType: ValueType]()
+        return reduce(&indexed) { $0[$1[keyPath: keyPath]] = $1[keyPath: valuePath] }
     }
 
     // MARK: - Indexed methods but throwing
@@ -416,9 +411,8 @@ public extension Array {
     func indexed<H>(
         by keySelector: (Element) throws -> H
     ) rethrows -> [H: Element] where H: Hashable {
-        var indexed: [H: Element] = [:]
-        try forEach { try indexed[keySelector($0)] = $0 }
-        return indexed
+        var indexed = [H: Element]()
+        return try reduce(&indexed) { try $0[keySelector($1)] = $1 }
     }
 
     @inlinable
@@ -426,9 +420,8 @@ public extension Array {
         by keySelector: (Element) throws -> KeyType,
         valueSelector: (Element) throws -> ValueType
     ) rethrows -> [KeyType: ValueType] where KeyType: Hashable {
-        var indexed: [KeyType: ValueType] = [:]
-        try forEach { try indexed[keySelector($0)] = valueSelector($0) }
-        return indexed
+        var indexed = [KeyType: ValueType]()
+        return try reduce(&indexed) { try $0[keySelector($1)] = valueSelector($1) }
     }
 
     @inlinable
@@ -436,8 +429,102 @@ public extension Array {
         by keyPath: KeyPath<Element, KeyType>,
         valueSelector: (Element) throws -> ValueType
     ) rethrows -> [KeyType: ValueType] where KeyType: Hashable {
-        var indexed: [KeyType: ValueType] = [:]
-        try forEach { try indexed[$0[keyPath: keyPath]] = valueSelector($0) }
-        return indexed
+        var indexed = [KeyType: ValueType]()
+        return try reduce(&indexed) { try $0[$1[keyPath: keyPath]] = valueSelector($1) }
+    }
+
+    // MARK: - Grouping methods but async
+
+    @inlinable
+    func grouped<H>(
+        by keySelector: (Element) async throws -> H
+    ) async rethrows -> [H: [Element]] where H: Hashable {
+        try await .init(grouping: self, by: keySelector)
+    }
+
+    @inlinable
+    func grouped<KeyType, ValueType>(
+        by keySelector: (Element) async throws -> KeyType,
+        valueSelector: (Element) async throws -> ValueType
+    ) async rethrows -> [KeyType: [ValueType]] where KeyType: Hashable {
+        try await grouped(by: keySelector) { try await $0.map(valueSelector) }
+    }
+
+    @inlinable
+    func grouped<KeyType, ResultType>(
+        by keySelector: (Element) async throws -> KeyType,
+        sink: ([Element]) async throws -> ResultType
+    ) async rethrows -> [KeyType: ResultType] where KeyType: Hashable {
+        try await grouped(by: keySelector).mapValues(sink)
+    }
+
+    @inlinable
+    func grouped<KeyType, ValueType>(
+        by keyPath: KeyPath<Element, KeyType>,
+        sink: ([Element]) async throws -> ValueType
+    ) async rethrows -> [KeyType: ValueType] where KeyType: Hashable {
+        try await grouped(by: keyPath).mapValues(sink)
+    }
+
+    @inlinable
+    func grouped<KeyType, InterimType, ResultType>(
+        by keyPath: KeyPath<Element, KeyType>,
+        extracting valuePath: KeyPath<Element, InterimType>,
+        sink: ([InterimType]) async throws -> ResultType
+    ) async rethrows -> [KeyType: ResultType] where KeyType: Hashable {
+        try await grouped(by: keyPath, extracting: valuePath).mapValues(sink)
+    }
+
+    @inlinable
+    func groupedFilter<H>(
+        by keySelector: (Element) async throws -> H,
+        filter: (Element) async throws -> Bool
+    ) async rethrows -> [H: [Element]] where H: Hashable {
+        try await grouped(by: keySelector) { try await $0.filter(filter) }
+    }
+
+    @inlinable
+    func groupedNilter<KeyType, OptionalType>(
+        by keySelector: (Element) async throws -> KeyType,
+        nilter: (Element) async throws -> OptionalType?
+    ) async rethrows -> [KeyType: [Element]] where KeyType: Hashable {
+        try await grouped(by: keySelector) { try await $0.nilter(nilter) }
+    }
+
+    @inlinable
+    func groupedReduce<KeyType, ResultType>(
+        _ initialValue: ResultType,
+        keySelector: (Element) async throws -> KeyType,
+        reducer: (ResultType, Element) async throws -> ResultType
+    ) async rethrows -> [KeyType: ResultType] where KeyType: Hashable {
+        try await grouped(by: keySelector) { try await $0.reduce(initialValue, reducer) }
+    }
+
+    // MARK: - Indexed methods but async
+
+    @inlinable
+    func indexed<H>(
+        by keySelector: (Element) async throws -> H
+    ) async rethrows -> [H: Element] where H: Hashable {
+        var indexed = [H: Element]()
+        return try await reduce(&indexed) { try await $0[keySelector($1)] = $1 }
+    }
+
+    @inlinable
+    func indexed<KeyType, ValueType>(
+        by keySelector: (Element) async throws -> KeyType,
+        valueSelector: (Element) async throws -> ValueType
+    ) async rethrows -> [KeyType: ValueType] where KeyType: Hashable {
+        var indexed = [KeyType: ValueType]()
+        return try await reduce(&indexed) { try await $0[keySelector($1)] = valueSelector($1) }
+    }
+
+    @inlinable
+    func indexed<KeyType, ValueType>(
+        by keyPath: KeyPath<Element, KeyType>,
+        valueSelector: (Element) async throws -> ValueType
+    ) async rethrows -> [KeyType: ValueType] where KeyType: Hashable {
+        var indexed = [KeyType: ValueType]()
+        return try await reduce(&indexed) { try await $0[$1[keyPath: keyPath]] = valueSelector($1) }
     }
 }
