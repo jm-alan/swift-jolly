acb2b717d36b7d0f2d1d443b26a6a0ce63fce1f9
JM <j.maxwell.alan@gmail.com>
2023-12-06 18:51:21 -0500

    create html serialization error struct

---
+++ Sources/Jolly/Utilities/HTML/HTMLSerializationError.swift
@@ -0,0 +1,24 @@
+public struct HTMLSerializationError: Error {
+    let tag: HTMLTag
+    let children: [HTMLTag]
+    let message: String
+
+    public init(_ reason: Reason, tag: HTMLTag, children: [HTMLTag]) {
+        self.tag = tag
+        self.children = children
+        message = reason.toString()
+    }
+
+    public enum Reason {
+        case voidChildren
+
+        @inlinable
+        @inline(__always)
+        public func toString() -> String {
+            switch self {
+            case .voidChildren:
+                return "Void tag must not have children"
+            }
+        }
+    }
+}
