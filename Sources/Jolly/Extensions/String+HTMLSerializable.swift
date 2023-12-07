07083e5f7fd685a680aeb64a10afc701fd5120a8
JM <j.maxwell.alan@gmail.com>
2023-12-06 22:30:54 -0500

    update String HTMLSerializable conformance implementation

--- Sources/Jolly/Extensions/String+HTMLSerializable.swift
+++ Sources/Jolly/Extensions/String+HTMLSerializable.swift
@@ -5,8 +5,22 @@ extension String: HTMLSerializable {
     public var innerText: String { self }
 
     @inlinable
-    public func innerHTML(pretty _: Bool) -> String { self }
+    public func innerHTML(pretty: Bool, depth: Int) -> String {
+        let indentPrefix = expandIndentation(depth: depth, spacing: 4)
+
+        return pretty
+            ? """
+
+            \(indentPrefix)\(replacingOccurrences(
+                of: "\n",
+                with: "\(indentPrefix)\n"
+            ))
+            """
+            : self
+    }
 
     @inlinable
-    public func serialize(pretty _: Bool) -> String { self }
+    public func serialize(pretty: Bool, depth: Int) -> String {
+        innerHTML(pretty: pretty, depth: depth)
+    }
 }
