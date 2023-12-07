6a4e17943c40373b8de05552e78160477007a8e0
JM <j.maxwell.alan@gmail.com>
2023-12-06 22:28:46 -0500

    make expandIndentation take in depth by name

--- Sources/Jolly/Utilities/expandIndentation.swift
+++ Sources/Jolly/Utilities/expandIndentation.swift
@@ -1,6 +1,6 @@
 @inlinable
 @inline(__always)
-public func expandIndentation(_ depth: Int, spacing: Int = 2) -> String {
+public func expandIndentation(depth: Int, spacing: Int = 2) -> String {
     var composed = ""
 
     for _ in 0..<depth {
