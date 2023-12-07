4791fa8a1be2b9dcfd037e894df3c514b8ea39d2
JM <j.maxwell.alan@gmail.com>
2023-12-06 22:31:34 -0500

    create rudimentary implementation of HTML document wrapper struct

---
+++ Sources/Jolly/Utilities/HTML/HTMLDocument.swift
@@ -0,0 +1,26 @@
+public struct HTMLDocument {
+    public var head: [HTMLElement]
+    public var body: HTMLElement?
+
+    @inlinable
+    @inline(__always)
+    public func serialize(pretty: Bool = false) throws -> String {
+        var composed = try head.reduce("") { try $0 + $1.serialize(pretty: pretty) }
+
+        try composed += body?.serialize(pretty: pretty, depth: 1) ?? ""
+
+        return pretty
+            ? """
+            <!DOCTYPE html>
+            <html>\
+            \(composed)
+            </html>
+            """
+            : "<!DOCTYPE html><html>\(composed)</html>"
+    }
+
+    public init(head: [HTMLElement] = .init(), body: HTMLElement? = nil) {
+        self.head = head
+        self.body = body
+    }
+}
