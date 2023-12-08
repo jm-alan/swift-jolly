2970d1749e891a00684046b0fe647f4bc5946fd6
JM <j.maxwell.alan@gmail.com>
2023-12-08 06:50:40 -0500

    add doc comments to divrem

--- Sources/Jolly/Utilities/Operators/DivRem.swift
+++ Sources/Jolly/Utilities/Operators/DivRem.swift
@@ -1,6 +1,9 @@
 infix operator /%
 
-/// Returns the quotient and remainder resulting from dividing the left hand side by the right.
+/// Returns the quotient and remainder resulting from dividing the left hand side by the right
+///
+///     let (quotient, remainder) = 13 /% 6
+///     print(quotient, remainder) // 2, 1
 @inlinable
 @inline(__always)
 public func /% (lhs: Int, rhs: Int) -> (Int, Int) { lhs.quotientAndRemainder(dividingBy: rhs) }
