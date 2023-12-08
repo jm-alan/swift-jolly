da28423ce01b2c99a08e258794ca0c092c47c998
JM <j.maxwell.alan@gmail.com>
2023-12-08 06:25:12 -0500

    simplify AtomicValue Comparable conformance

--- Sources/Jolly/Utilities/AtomicValue/AtomicValue+Comparable.swift
+++ Sources/Jolly/Utilities/AtomicValue/AtomicValue+Comparable.swift
@@ -4,39 +4,19 @@ extension AtomicValue: Comparable where Wrapped: Comparable {
     public static func < (
         lhs: AtomicValue<Wrapped>,
         rhs: AtomicValue<Wrapped>
-    ) -> Bool {
-        var result: Bool!
-        lhs.use { leftVal in
-            rhs.use { rightVal in
-                result = leftVal < rightVal
-            }
-        }
-        return result
-    }
+    ) -> Bool { lhs.use { leftVal in rhs.use { leftVal < $0 } } }
 
     @inlinable
     @inline(__always)
     public static func < (
         lhs: AtomicValue<Wrapped>,
         rhs: Wrapped
-    ) -> Bool {
-        var result: Bool!
-        lhs.use { leftVal in
-            result = leftVal < rhs
-        }
-        return result
-    }
+    ) -> Bool { lhs.use { $0 < rhs } }
 
     @inlinable
     @inline(__always)
     public static func < (
         lhs: Wrapped,
         rhs: AtomicValue<Wrapped>
-    ) -> Bool {
-        var result: Bool!
-        rhs.use { rightVal in
-            result = lhs < rightVal
-        }
-        return result
-    }
+    ) -> Bool { rhs.use { lhs < $0 } }
 }
