f89ec861e25e476d53ad5ee13bf5aa13e7695848
JM <j.maxwell.alan@gmail.com>
2023-12-08 06:37:43 -0500

    create inlinable min and max operators

---
+++ Sources/Jolly/Utilities/Operators/MinMax.swift
@@ -0,0 +1,18 @@
+infix operator <>
+infix operator ><
+
+/// Returns the smaller of two values
+///
+///     let min = 7 <> 6
+///     print(min) // 6
+@inlinable
+@inline(__always)
+public func <> <C>(lhs: C, rhs: C) -> C where C: Comparable { lhs < rhs ? lhs : rhs }
+
+/// Returns the larger of two values
+///
+///     let max = 7 <> 6
+///     print(max) // 7
+@inlinable
+@inline(__always)
+public func >< <C>(lhs: C, rhs: C) -> C where C: Comparable { lhs < rhs ? rhs : lhs }
